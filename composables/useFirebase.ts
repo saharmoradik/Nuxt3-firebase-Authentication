78fbee20ca91191454a580db85a70d70aaafe79f
saharmoradik <saharmoradik@gmail.com>
2022-10-28 12:46:58 +0200

    firebase refactory

---
+++ composables/useFirebase.ts
@@ -0,0 +1,55 @@
+//https://firebase.google.com/docs/auth/web/start?hl=en&authuser=0
+
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+//------------Sign up new users------------------------------
+
+export const createUser = async (email, password) => {
+  const auth = getAuth();
+  const credentials = await createUserWithEmailAndPassword(
+    auth,
+    email,
+    password
+  ).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+  return credentials;
+};
+
+//------------Sign in existing users-----------------------
+
+export const signInUser = async (email, password) => {
+  const auth = getAuth();
+  const credentials = await signInWithEmailAndPassword(
+    auth,
+    email,
+    password
+  ).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+  return credentials;
+};
+
+//------------------state observer-------------------
+
+export const initUser = async () => {
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      const uid = user.uid;
+      console.log(user);
+    } else {
+      // User is signed out
+      // ...
+    }
+  });
+};
