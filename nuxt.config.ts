64aa0ae7782e05acf458b4b73a0e0dc85656d62b
saharmoradik <saharmoradik@gmail.com>
2022-10-28 11:14:43 +0200

    initial setting-firebase and nuxt3

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+import { defineNuxtConfig } from "nuxt/config";
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  publicRuntimeConfig: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+  },
+  privateRuntimeConfig: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+  },
+});
---
+++ plugins/firebaseAuth.client..ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const config = useRuntimeConfig();
+
+  const firebaseConfig = {
+    apiKey: config.FIREBASE_API_KEY,
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+});
