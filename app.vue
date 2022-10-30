c685af6db7393011a9f44d54f707d2c9bd162eba
saharmoradik <saharmoradik@gmail.com>
2022-10-30 13:41:54 +0100

    add user credential to page

--- app.vue
+++ app.vue
@@ -3,21 +3,22 @@
     <v-alert type="info">Welcome Vuetify </v-alert>
     <v-btn @click="singIn">Sign in</v-btn>
     <v-btn @click="singOut">Sign out</v-btn>
-
+    <pre>
+      {{ credentials }}
+    </pre>
     <br />
   </div>
 </template>
 
 <script setup>
+const credentials = ref();
 const singIn = async () => {
   const email = "sahsasrcmoradiuk@gmail.com";
   const password = "123456";
-  const credentials = await signInUser(email, password);
-  console.log("credential", credentials);
+  credentials.value = await signInUser(email, password);
 };
 const singOut = async () => {
-  const result = await signOutUser();
-  console.log(result);
+  credentials.value = await signOutUser();
 };
 //onMounted(async () => {});
 </script>
